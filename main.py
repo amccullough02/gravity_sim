6e205c6b37806c94bacf1ee61dfd876b70637771
Alex McCullough <alexmccullough@outlook.com>
2024-09-12 15:48:39 +0100

    feat: created window

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF, vsync=1)
+pygame.display.set_caption("N-body Sim")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
